8cea19032839346c895f4123c716f4c2cd43b80b
Iñiguez Guizar Erandy Sirein <iniguez.guizar.erandy.sirein@gmail.com>
2021-11-07 20:28:12 -0600

    Segunda versión

---
+++ app/src/main/java/com/erandy/sirein/iniguez/guizar/calculadoradis/Calculadora.java
@@ -0,0 +1,27 @@
+package com.erandy.sirein.iniguez.guizar.calculadoradis;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable {
+
+    public void suma()
+    {
+
+    }
+
+    public void resta()
+    {
+
+    }
+
+    public void multiplicacion()
+    {
+
+    }
+
+    public void division()
+    {
+
+    }
+
+}
